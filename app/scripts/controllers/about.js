ef2c0c025aa32e5cf951127c5d9cdbd680e8f21a
jnr-mader <jnr.mader@gmail.com>
2015-01-24 17:49:02 +0000

    first upload

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gufufuApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gufufuApp
+ */
+angular.module('gufufuApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gufufuApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gufufuApp
+ */
+angular.module('gufufuApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
